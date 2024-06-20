be3e02d8ae5d31d41d6fdaded4388c91006f8c1a
MAKI\jtelleria <jtelleriat@outlook.com>
2024-06-19 18:41:07 -0600

    Movement and animations added

--- BGS_Shop/Assets/Scripts/PlayerControls.cs
+++ BGS_Shop/Assets/Scripts/PlayerControls.cs
@@ -1,3 +1,5 @@
+using System;
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
@@ -12,11 +14,14 @@ public class PlayerControls : MonoBehaviour
     
     private PlayerInputActions _playerInputActions;
     private InputAction _moveInput;
+    private InputAction _runInput;
 
     [Header("Movement")]
     [SerializeField] private float _walkSpeed = 200f;
-    [SerializeField] private float _runSpeed = 400f;
-
+    [SerializeField] private float _runSpeedMultiplier = 1.8f;
+    
+    [Header("Animation")]
+    [SerializeField]private GameObject _parentRenderer;
     private void Awake()
     {
         _playerInput = GetComponent<PlayerInput>();
@@ -34,29 +39,53 @@ public class PlayerControls : MonoBehaviour
     {
         _playerInputActions.Player.Interact.performed += Interact;
         _playerInputActions.Player.Interact.Enable();
+        
         _moveInput = _playerInputActions.Player.Move;
         _moveInput.Enable();
+        
+        _runInput = _playerInputActions.Player.Run;
+        _runInput.Enable();
     }
     
-    void Start()
-    {
-    }
 
     private void Interact(InputAction.CallbackContext obj)
     {
         Debug.Log("Interacting");
     }
+    
 
     private void FixedUpdate()
     {
         Move();
+        SelectAnimation();
     }
+
 
     private void Move()
     {
+        _rb.velocity = CalculateVelocity();
+    }
+
+    private Vector2 CalculateVelocity()
+    {
+        //assigning the speed multiplier
+        float speedMultiplier = _runInput.IsPressed() ? _runSpeedMultiplier : 1;
+        
+        //reading direction input
         Vector2 moveVelocity = _moveInput.ReadValue<Vector2>();
-        moveVelocity *= (_walkSpeed * Time.deltaTime);
-        _rb.velocity = moveVelocity;
+        moveVelocity *= (_walkSpeed * Time.deltaTime * speedMultiplier);
+        
+        return moveVelocity;
+    }
+    private void SelectAnimation()
+    {
+        _animator.SetFloat("MovementSpeed",_rb.velocity.magnitude);
+
+        if (_rb.velocity.magnitude < 0.1f) return;
+        
+        //checking if we are moving left
+        int lookleft = _rb.velocity.x > 0 ? 1 : -1;
+        _parentRenderer.transform.localScale =new Vector3(lookleft, 1, 1);
     }
 
     public void OnInteract(InputAction.CallbackContext context)
